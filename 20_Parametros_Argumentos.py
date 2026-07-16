06070a55816b5b9e2990d9eaea2a674b66a3b727
Arthur Mariano <thiesto@gmail.com>
2026-07-16 14:08:22 -0300

    Testando refatoração pelo o VS code

--- 20_Parametros_Argumentos.py
+++ 20_Parametros_Argumentos.py
@@ -6,19 +6,19 @@ Functions (Funções)
 
 def boas_vindas_Jorgina():
   print('Olá, seja bem-vindo, Jorgina!')
-  print('Temos 5 laptops em estoque')
+  print('Temos 5 laptops em estoque!')
   print('----------------------------------')
 
 
 def boas_vindas_Mirian():
   print('Olá, seja bem-vindo, Mirian!')
-  print('Temos 4 laptops em estoque')
+  print('Temos 4 laptops em estoque!')
   print('----------------------------------')
 
 
 def boas_vindas_Joao():
   print('Olá, seja bem-vindo, João!')
-  print('Temos 3 laptops em estoque')
+  print('Temos 3 laptops em estoque!')
   print('----------------------------------')
 
 boas_vindas_Jorgina()
