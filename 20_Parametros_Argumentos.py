246610bdf9cce9dc5b73937b84e24f0e49fd892d
Arthur Mariano <thiesto@gmail.com>
2026-07-16 14:03:22 -0300

    Preparando funções

---
+++ 20_Parametros_Argumentos.py
@@ -0,0 +1,10 @@
+'''
+Functions (Funções)
+  # DRY - Don't repeat yourself 
+  # Parâmetro --> Argumento
+'''
+
+def boas_vindas():
+  print('Olá, seja bem-vindo, Jorgina!')
+  print('Temos 5 laptops em estoque')
+boas_vindas()
