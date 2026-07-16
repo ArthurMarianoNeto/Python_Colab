406829453fe4eb97a84026212b02390519b0f22b
Arthur Mariano <thiesto@gmail.com>
2026-07-16 14:13:13 -0300

    Versão final, antes e depois das funções

--- 20_Parametros_Argumentos.py
+++ 20_Parametros_Argumentos.py
@@ -3,24 +3,36 @@ Functions (Funções)
   # DRY - Don't repeat yourself 
   # Parâmetro --> Argumento
 '''
+def boas_vindas(nome, quantidade):
+  print(f'Olá, seja bem-vindo, {nome}!')
+  print(f'Temos {str(quantidade)} laptops em estoque!')
+  print('----------------------------------')
 
+boas_vindas('Jorgina', 4)
+boas_vindas('Mirian', 3)
+boas_vindas('João', 2)
+
+
+
+'''
 def boas_vindas_Jorgina():
   print('Olá, seja bem-vindo, Jorgina!')
-  print('Temos 5 laptops em estoque!')
+  print('Temos 5 laptops em estoque')
   print('----------------------------------')
 
 
 def boas_vindas_Mirian():
   print('Olá, seja bem-vindo, Mirian!')
-  print('Temos 4 laptops em estoque!')
+  print('Temos 4 laptops em estoque')
   print('----------------------------------')
 
 
 def boas_vindas_Joao():
   print('Olá, seja bem-vindo, João!')
-  print('Temos 3 laptops em estoque!')
+  print('Temos 3 laptops em estoque')
   print('----------------------------------')
 
 boas_vindas_Jorgina()
 boas_vindas_Mirian()
 boas_vindas_Joao()
+'''
